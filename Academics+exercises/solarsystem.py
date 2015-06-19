ee0bbbeb10d68302f1ce0a336a0ab88a4237fae0
Maurice Wilson <Ml_wilson1@yahoo.com>
2015-06-19 10:27:12 -0400

    first attempt at first cell in pynb

---
+++ Academics+exercises/solarsystem.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+'''Some fun with object-oriented programming: make a file called ``solarsystem.py`` that defines ``Star``, ``Planet``, and ``System`` objects that enable the provided notebook file to work as demonstrated.  If you finish this, feel free to add more functionality to these objects! '''
+
+Class Star(object):
+    def __init__(self,Name='Sun')
+        self.Name = Name
+        if Name == 'Sun' or Name == 'sun':
+            Mass = 1.00
+            Radius = 1.00
+            print('Sol: '+str(Mass)+' M_'+Name+', '+str(Radius)+' R_'+Name
+        #return
