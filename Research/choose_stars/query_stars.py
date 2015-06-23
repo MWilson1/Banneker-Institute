d0dae33eb85408ac8cdafaae0871f6f7d38c42d1
Maurice Wilson <Ml_wilson1@yahoo.com>
2015-06-23 12:19:33 -0400

    unfinished exercise

--- Research/choose_stars/query_stars.py
+++ Research/choose_stars/query_stars.py
@@ -26,11 +26,12 @@ import numpy as np
 
 from astroquery.simbad import Simbad
 
+import json, collections, datetime
 
+infile = sys.argv[1]
 
-with open(str(sys.argv[1]), 'r') as p_star_file:
+with open(infile, 'r') as p_star_file:
     file_strings = p_star_file.readlines()
-
 #p_star_file.close() # this is unnecessary
 
 # find which line in the file starts the table
@@ -74,7 +75,7 @@ p_star_table = Table([p_star_indice, p_star_id, p_star_spec, p_star_mass, p_star
 # customize the output of the query results
 customS = Simbad()
 customS.remove_votable_fields('coordinates')
-customS.add_votable_fields('coo(s)')
+customS.add_votable_fields('coo(d)')
 customS.add_votable_fields('flux(B)')
 customS.add_votable_fields('flux(V)')
 customS.add_votable_fields('otype(V)')
@@ -90,18 +91,75 @@ mags_V = [p_star_table['V mag'][p_star] for p_star in xrange(len(p_star_table['I
 mags_V_p = np.array(mags_V)+mag_limit
 mags_V_m = np.array(mags_V)-mag_limit
 
-  # determine B - V color of primary star
-    # add/subtract to find upper and lower limits of color
-                  #PROBABLY UNNECESSARY
-                  
-  # criteria-query for comparison stars based on the parameters: V magnitude, vicinity, and if it is not variable, (and maybe color)
-comparison_S_tables =  [customS.query_criteria('region(circle, '+p_star_table['ID'][star]+', 5d)', 'maintypes=*', 'maintypes!=V*', 'Vmag < '+str(mags_V_p[star]), 'Vmag > '+str(mags_V_m[star])) for star in xrange(len(p_star_table['ID']))]
+# determine B - V color of primary star
+# add/subtract to find upper and lower limits of color
+#PROBABLY UNNECESSARY
+
+# criteria-query for comparison stars based on the parameters: V magnitude, vicinity, and if it is not variable, (and maybe color)
+def find_comparison(primary_S_indice):
+    return customS.query_criteria('region(circle, '+p_star_table['ID'][primary_S_indice]+', 5d)', 'maintypes=*', 'maintypes!=V*', 'Vmag < '+str(mags_V_p[primary_S_indice]), 'Vmag > '+str(mags_V_m[primary_S_indice]))
+
+decision = raw_input('Do you want to search for a comparison star for all '+str(len(p_star_table['ID']))+' primary target stars?  (Type 1)\nOr do you want to only search around 1 primary target star for a comparison star (Type 2)\n---> ')
+
+
+if decision == '1':
+    comparison_S_tables =  [customS.query_criteria('region(circle, '+p_star_table['ID'][star]+', 5d)', 'maintypes=*', 'maintypes!=V*', 'Vmag < '+str(mags_V_p[star]), 'Vmag > '+str(mags_V_m[star])) for star in xrange(len(p_star_table['ID']))]
+elif decision == '2':
+    primary_S_indice  = input('What is the indice of the primary star within the '+sys.argv[1]+' file?\n---> ')
+    comparison_table = find_comparison(primary_S_indice)
+    print(comparison_table)
+else:
+    print('You have typed an invalid number.  Crashing program...')
+    2*'hey'
+
+# input the primary and comparison stars into file formatted at a readable Target List
+
+
+telnum = 1
+
+tonight = datetime.date.today() + datetime.timedelta(days=1)
+# specify the calibrations
+filename = datetime.datetime.strftime(tonight,'n%Y%m%d') + '.T' + str(telnum) + '.txt'
+with open(filename, "w") as outfile:
+    outfile.write('{"nbias": 11, "ndark": 11, "nflat": 9, "darkexptime": [300], "flatFilters": ["ip","R"]\, "WaitForMorning": true}\n')
+    outfile.write('{"nbiasEnd": 11, "ndarkEnd": 11, "nflatEnd": 9}\n')
+    
+    
+if telnum == 1:
+    name = p_star_table['ID'][0]
+    rahrs = p_star_table['RA (deg)'][0]
+    decdegs = p_star_table['Dec (deg)'][0]
+elif telnum == 2:
+    name = comparison_S_tables[0]['MAIN_ID'][0]
+    rahrs = comparison_S_tables[0]['RA_d'][0]
+    decdegs = comparison_S_tables[0]['DEC_d'][0]
+    
+    
+# Add a target to the file:
+target = collections.OrderedDict()
+target['name'] = name
+target['ra'] = rahrs
+target['dec'] = decdegs
+target['starttime'] = datetime.datetime.strftime(starttime.datetime(),'%Y-%m-%d %H:%M:%S')
+target['endtime'] = datetime.datetime.strftime(endtime.datetime(),'%Y-%m-%d %H:%M:%S')
+target['filter'] = ["ip"]
+target['exptime'] = [300]
+target['num'] = [1]
+target['defocus'] = 0.0
+target['selfguide'] = True
+target['guide'] = False
+target['cycleFilter'] = True
+target['positionAngle'] = 0.0
+with open(filename, "a") as outfile:
+    json.dump(target,outfile)
+    outfile.write('\n')
+    
 
 
 
 
-
-
+#search_stars(infile)
+        
 '''    #Scraps
 
 # customize the output of the query results
