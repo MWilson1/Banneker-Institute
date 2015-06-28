717be0e5dfa191fc275c22bfa7993f3696399631
Maurice Wilson <Ml_wilson1@yahoo.com>
2015-06-27 22:55:07 -0400

    minor adjustments

--- Research/choose_stars/query_stars_def.py
+++ Research/choose_stars/query_stars_def.py
@@ -83,25 +83,34 @@ tonight = datetime.date.today() + datetime.timedelta(days=1)
 obs = ephem.Observer()
 obs.lat = '31.680407'
 obs.lon = '-110.878977'
-obs.horizon = '-12'
+obs.horizon = '-12'    # Nautical Twilight
 
 today = datetime.datetime.utcnow()   # UTC TIME !!!!!        Arizona time is UTC - 7 hours             # UTC TIME !   EST is UTC - 6 hours (daylight savings time)!!!
 startNightTime = datetime.datetime(today.year, today.month, today.day, 17) - datetime.timedelta(days=1)
 
-# sunrise/sunset times
 sunrise = obs.next_rising(ephem.Sun(), start=startNightTime,use_center=True).datetime()
 sunset = obs.next_setting(ephem.Sun(), start=startNightTime,use_center=True).datetime()
 
+subsequent_sunrise = obs.next_rising(ephem.Sun(), start=startNightTime + datetime.timedelta(days=1),use_center=True).datetime()
+subsequent_sunset = obs.next_setting(ephem.Sun(), start=startNightTime + datetime.timedelta(days=1),use_center=True).datetime()
 
-
+if subsequent_sunrise > subsequent_sunset:
+    not_vis_latter = obs.next_rising(ephem.Sun(), start=startNightTime + datetime.timedelta(days=2),use_center=True).datetime() # latest sunrise
+    not_vis_former = subsequent_sunrise
+else:
+    not_vis_latter = obs.next_setting(ephem.Sun(), start=startNightTime + datetime.timedelta(days=1),use_center=True).datetime() # latest sunset
+    not_vis_former = subsequent_sunset
 
     # Go through each primary target star and find its rise and set time.
 def choose_primary(p_star_table, p_star_indice=0):        
     name = p_star_table['ID'][p_star_indice]
     rahrs = p_star_table['RA (deg)'][p_star_indice]
     decdegs = p_star_table['Dec (deg)'][p_star_indice]
+    
     # compute the rise/set times of the target
-    obs.horizon = '30.0'
+    
+    obs.horizon = '30.0' 
+    
     body = ephem.FixedBody()
     #body._ra = str(target['ra'])
     #body._dec = str(target['dec'])
@@ -109,20 +118,26 @@ def choose_primary(p_star_table, p_star_indice=0):
     body._dec = str(decdegs)
     body._epoch = '2000.0'
     body.compute()
-    
+
 
     observable = 'Not sure'
     time_l_visible = None
     start_time = None
-    #subsequent_sunrise = None
+    end_time = None
     
     # the "NeverUpError" can occur so I need this try statement
     try:
         risetime = obs.next_rising(body,start=startNightTime).datetime() # start=startNightTime + datetime.timedelta(days=1)    
         settime = obs.next_setting(body,start=startNightTime).datetime()
+        try_transit = obs.next_transit(body, start=startNightTime).datetime()
         
     except ephem.NeverUpError:
         print('The NeverUpError occurred for p_star_indice = '+str(p_star_indice))
+        observable = False
+        return observable, time_l_visible
+    
+    except ephem.AlwaysUpError:
+        print('The AlwaysUpError occurred for p_star_indice = '+str(p_star_indice))
         observable = False
         return observable, time_l_visible
 
@@ -130,65 +145,78 @@ def choose_primary(p_star_table, p_star_indice=0):
     def longer_than_24hrs_and_Diff(diff_of_times): # must be a datetime type
         return np.abs(diff_of_times.days*24.0 + diff_of_times.seconds/3600.00) > 24.0 , diff_of_times.days*24.0 + diff_of_times.seconds/3600.00
 
-
-    #if risetime < today: risetime = obs.next_rising(body,start=startNightTime + datetime.timedelta(days=1)).datetime()
-    subsequent_sunrise = obs.next_rising(ephem.Sun(), start=startNightTime + datetime.timedelta(days=1),use_center=True).datetime()
-
-    #if p_star_indice == 116: print(subsequent_sunrise)
-    
-    '''
-    sunset_counter = 0
-    while sunset < sunrise:
-        sunset = obs.next_setting(ephem.Sun(), start=startNightTime + datetime.timedelta(days=sunset_counter), use_center=True).datetime() #if so, then find the very next sunset time
-        sunset_counter += 1
-    '''
     settime_counter = 0
     while settime < today:
         settime = obs.next_setting(body,start=startNightTime + datetime.timedelta(days=settime_counter)).datetime()   #if so, then find the very next settime
         settime_counter += 1
     if settime < risetime: settime = obs.next_setting(body,start=startNightTime + datetime.timedelta(days=1)).datetime()   #if so, then find the very next settime
     
-    if longer_than_24hrs_and_Diff(settime - risetime)[1] < 8760.0 and longer_than_24hrs_and_Diff(settime - risetime)[0] == True: risetime = obs.next_rising(body,start=startNightTime + datetime.timedelta(days=1)).datetime()   # if difference is less than a year but more than a day, then find the next risetime
+    if longer_than_24hrs_and_Diff(settime - risetime)[1] < 8760.0 and longer_than_24hrs_and_Diff(settime - risetime)[0] == True:
+        risetime = obs.next_rising(body,start=startNightTime + datetime.timedelta(days=1)).datetime()   # if difference is less than a year but more than a day, then find the next risetime.  This might be useful because of how many times (settime_counter) I might have changed the settime.  However, it is more than likely that settime_counter will , at most, equal 2. Therefore, I will have only need to bump up risetime once, which is exactly what I am doing.
+        try_transit = obs.next_transit(body, start=startNightTime + datetime.timedelta(days=1)).datetime()
+        
     
-    # at this point settime is after RIGHT NOW-time and after the risetime.   AND sunset is after sunrise time.
+    # at this point settime is after RIGHT NOW-time and after the risetime.  AND sunrise can be after sunset or before sunset
      
     
     if longer_than_24hrs_and_Diff(settime - risetime)[0] == True: observable = False # if the set and rise times are more than one day apart, then this star won't be visible until a year later
+
+    if risetime < sunset and risetime > sunrise and settime < sunset and settime > sunrise: observable = False
+
+    if risetime < subsequent_sunset and risetime > sunrise and settime < subsequent_sunset and settime > sunrise: observable = False
+
+    if risetime > not_vis_former and risetime < not_vis_latter and settime > not_vis_former and settime < not_vis_latter: observable = False
+
     
     # Determine if star is visible between sunset and sunrise.  Also, find the length of time it is visible.
     if observable != False:
-        if settime < sunset:
-            observable = False
-        else:     # at this point settime is after RIGHT NOW-time, after the risetime, and after sunset time.   AND sunset is after sunrise time.
-            observable = True
-            if longer_than_24hrs_and_Diff(risetime - sunset)[1] > 0:     # if this Diff is positive, the risetime is after sunset
-                
-                if np.abs(longer_than_24hrs_and_Diff(subsequent_sunrise - risetime)[1]) > np.abs(longer_than_24hrs_and_Diff(settime - risetime)[1]):
-                    time_l_visible = longer_than_24hrs_and_Diff(settime - risetime)[1]
-                else:
-                    time_l_visible = longer_than_24hrs_and_Diff(subsequent_sunrise - risetime)[1]
-                    
+        # at this point settime is after RIGHT NOW-time, after the risetime, and after sunset time.   
+        observable = True
+        if longer_than_24hrs_and_Diff(risetime - sunset)[1] > 0:     # if this Diff is positive, the risetime is after sunset (but BEFORE subsequent_sunset)
+            find_cases = np.array([np.abs(longer_than_24hrs_and_Diff(settime - risetime)[1]), np.abs(longer_than_24hrs_and_Diff(subsequent_sunrise - risetime)[1]), np.abs(longer_than_24hrs_and_Diff(settime - subsequent_sunset)[1]), np.abs(longer_than_24hrs_and_Diff(sunrise - risetime)[1]) ])
+            
+            case = np.where(find_cases == np.min(find_cases))[0][0]
+
+            time_l_visible = find_cases[case]
+            if case == 0:
                 start_time = risetime
+                end_time = settime
+                    
+            elif case == 1:                
+                start_time = risetime
+                end_time = subsequent_sunrise
                 
-            else:                                                        # the risetime is before sunset
-                time_l_visible = longer_than_24hrs_and_Diff(settime - sunset)[1]
-                start_time = sunset
+            elif case == 2:
+                start_time = subsequent_sunset
+                end_time = settime
+                
+            elif case == 3:
+                start_time = risetime
+                end_time = sunrise
+                    
+        else:                                                        # the risetime is before sunset
+            time_l_visible = longer_than_24hrs_and_Diff(settime - sunset)[1]
+            start_time = sunset
+            end_time = settime
+            
+    
+    #trans_meridian_time = datetime.timedelta(0, longer_than_24hrs_and_Diff(settime - risetime)[1] /2.0 *3600.00) + risetime
+    
+    #following_sunrise = obs.next_rising(ephem.Sun(), start=startNightTime + datetime.timedelta(days=2), use_center=True).datetime()
+    
+    #midnight_time = datetime.timedelta(0, longer_than_24hrs_and_Diff(subsequent_sunrise - sunset)[1] /2.0 *3600.00) + sunset
+    
+    #diff_transit_meridian_and_midnight = longer_than_24hrs_and_Diff(trans_meridian_time - midnight_time)[1]
 
     
-    trans_meridian_time = datetime.timedelta(0, longer_than_24hrs_and_Diff(settime - risetime)[1] /2.0 *3600.00) + risetime
-    
-    following_sunrise = obs.next_rising(ephem.Sun(), start=startNightTime + datetime.timedelta(days=2), use_center=True).datetime()
-    
-    midnight_time = datetime.timedelta(0, longer_than_24hrs_and_Diff(following_sunrise - sunset)[1] /2.0 *3600.00) + sunset
-    
-    diff_transit_meridian_and_midnight = longer_than_24hrs_and_Diff(trans_meridian_time - midnight_time)[1]
-    
-    #print('Sunrise : '+str(sunrise)+'\n\nSunset : '+str(sunset)+'\n\nMidnight : '+str(midnight_time)+'\n\n')
-    return observable, p_star_indice, time_l_visible, risetime, settime, diff_transit_meridian_and_midnight, trans_meridian_time, start_time
+    #if p_star_indice==116: print('Sunrise : '+str(sunrise)+'\n\nSunset : '+str(sunset)+'\n\nMidnight : '+str(midnight_time)+'\n\n')
+    return observable, p_star_indice, time_l_visible, risetime, settime, try_transit, start_time, end_time
 
 all_observables = [choose_primary(p_star_table, p_star_indice=iterate) for iterate in xrange(len(p_star_table)) if choose_primary(p_star_table, p_star_indice=iterate)[0]  == True]
 
-best_observables = [all_observables[iterate] for iterate in xrange(len(all_observables)) if all_observables[iterate][1] > 1.0 and np.abs(all_observables[iterate][5]) < 10.0]
+#best_observables = [all_observables[iterate] for iterate in xrange(len(all_observables)) if all_observables[iterate][2] > 1.0 and np.abs(all_observables[iterate][5]) < 10.0]
+
+best_observables = [all_observables[iterate] for iterate in xrange(len(all_observables)) if all_observables[iterate][2] > 1.0 ]
 
 #best_observables =  [choose_primary(p_star_table, p_star_indice=iterate) for iterate in [116, 0]]
 
@@ -245,12 +273,12 @@ comparison_S_tables = [find_comparison(best_observables[p_S_ind][1]) for p_S_ind
     else:
         print('You have typed an invalid number.  Crashing program...')
         return
- '''
+'''
 
 
 
 
-
+'''
 
 
 
@@ -266,7 +294,8 @@ for telnum in np.array([1, 2, 3, 4]):
         
     for ind in xrange(len(comparison_S_tables)):
         
-        '''
+'''
+'''
         year_s_e = [best_observables[ind][3].year, best_observables[ind][4].year]
         year_s_e = [2015, 2015]
         month_s_e = [06,06]
@@ -277,10 +306,13 @@ for telnum in np.array([1, 2, 3, 4]):
                     
         starttime = datetime.datetime(year_s_e[0],month_s_e[0],day_s_e[0],hour_s_e[0],minute_s_e[0])  # this is just risetime
         endtime = datetime.datetime(year_s_e[1],month_s_e[1],day_s_e[1],hour_s_e[1],minute_s_e[1])   # this is just endtime
-        '''
+'''
+'''
         
-        starttime = best_observables[ind][-1]
+        starttime = best_observables[ind][-2]
         endtime = starttime + datetime.timedelta(0, 1800.0, 000000)  # I just added by 30 minutes
+        endtime = best_observables[ind][-1]
+
         
         if telnum == 1:
             name = p_star_table['ID'][best_observables[ind][1]]
@@ -313,7 +345,7 @@ for telnum in np.array([1, 2, 3, 4]):
 
 
 
-        
+'''
         
 '''    
 #Scraps
